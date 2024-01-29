fded817aeec3b634cb5ce272978cacdc657871ee
François ROLAND <froland@users.noreply.github.com>
2024-01-29 10:11:02 +0000

    Implement list and add functionnalities.
    Co-authored-by: Zakaria-hak <Zakaria-hak@users.noreply.github.com>
    Co-authored-by: anouar51 <anouar51@users.noreply.github.com>
    Co-authored-by: ig-paty <ig-paty@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 0, name: "Créer l'état" },
+    { id: 1, name: 'Afficher les todos' },
+  ]);
+
+  const [text, setText] = useState('');
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    const newId = crypto.randomUUID();
+    const tDo = [...todos, { id: newId, name: text }];
+    setTodos(tDo);
+    setText("");
+  };
+
+  const handleChange = function (e) {
+    const val = e.target.value;
+    setText(val);
+  };
+
   return (
     <>
       <Hello />
+      <ul>
+        {todos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.id}</span>
+            <span>{toDo.name}</span>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="entrer une valeur"
+          onChange={handleChange}
+          value={text}
+        />
+        <button type="submit">Ajouter</button>
+      </form>
     </>
   );
 }
